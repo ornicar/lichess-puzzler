a071f15fc7cfe79d4805071b00d684dc87e034b9
kraktus <56031107+kraktus@users.noreply.github.com>
2020-11-04 16:05:47 +0000

    Refractor code for tagging mate in X puzzles

--- tagger/cook.py
+++ tagger/cook.py
@@ -100,13 +100,7 @@ def attraction(puzzle: Puzzle) -> bool:
 def mate_in(puzzle: Puzzle) -> Optional[TagKind]:
     if not puzzle.game.end().board().is_checkmate():
         return None
-    moves_to_mate = int(len(puzzle.mainline) / 2)
-    if moves_to_mate == 2:
-        return "mateIn2"
-    elif moves_to_mate == 3:
-        return "mateIn3"
-    elif moves_to_mate == 4:
-        return "mateIn4"
-    elif moves_to_mate == 5:
-        return "mateIn5"
+    moves_to_mate = len(puzzle.mainline) // 2
+    if moves_to_mate < 6:
+        return f"mateIn{moves_to_mate}"
     return "mateIn6+"
