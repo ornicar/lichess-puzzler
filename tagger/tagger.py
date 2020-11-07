5f5b5895b4612ee851bbb9c1b14ab99fcf71a859
Thibault Duplessis <t@lichess.org>
2020-11-07 17:47:05 +0100

    tagger multiprocessing

--- tagger/tagger.py
+++ tagger/tagger.py
@@ -2,6 +2,7 @@ import pymongo
 import logging
 import sys
 import argparse
+from multiprocessing import Pool
 import cook
 from chess import Move, Color, Board, WHITE, BLACK
 from chess.pgn import Game, GameNode
@@ -20,35 +21,40 @@ def read(doc) -> Puzzle:
         node = node.add_main_variation(move)
     return Puzzle(doc["_id"], node.game())
 
-def main() -> None:
-    # sys.setrecursionlimit(10000) # else node.deepcopy() sometimes fails?
-    parser = argparse.ArgumentParser(prog='tagger.py', description='automatically tags lichess puzzles')
-    parser.add_argument("--dry", "-d", help="dry run")
-    parser.add_argument("--verbose", "-v", help="increase verbosity", action="count")
-    args = parser.parse_args()
-    if args.verbose == 1:
-        logger.setLevel(logging.DEBUG)
-    mongo = pymongo.MongoClient()
-    db = mongo['puzzler']
-    puzzle_coll = db['puzzle2']
-    tag_coll = db['tag']
-    nb = 0
+# sys.setrecursionlimit(10000) # else node.deepcopy() sometimes fails?
+parser = argparse.ArgumentParser(prog='tagger.py', description='automatically tags lichess puzzles')
+parser.add_argument("--dry", "-d", help="dry run")
+parser.add_argument("--verbose", "-v", help="increase verbosity", action="count")
+args = parser.parse_args()
+if args.verbose == 1:
+    logger.setLevel(logging.DEBUG)
+mongo = pymongo.MongoClient()
+db = mongo['puzzler']
+puzzle_coll = db['puzzle2']
+tag_coll = db['tag']
+nb = 0
 
+def tags_of(doc) -> Tuple[Puzzle, List[TagKind]]:
+    puzzle = read(doc)
+    return puzzle.id, cook.cook(puzzle)
+
+with Pool(processes=8) as pool:
+    batch = []
     for doc in puzzle_coll.find():
-        puzzle = read(doc)
-        tags = cook.cook(puzzle)
-        if not args.dry:
-            ups = {}
-            for tag in tags:
-                if not tag in static_kinds:
-                    ups["{}+".format(tag)] = "lichess"
-            if ups:
-                tag_coll.update_one({"_id":puzzle.id},{"$addToSet":ups}, upsert = True)
-            if tags:
-                puzzle_coll.update_one({"_id":puzzle.id},{"$addToSet":{"tags":{"$each":tags}}})
-        nb = nb + 1
+        if len(batch) < 200:
+            batch.append(doc)
+            continue
+        for id, tags in pool.imap_unordered(tags_of, batch):
+            if not args.dry:
+                ups = {}
+                for tag in tags:
+                    if not tag in static_kinds:
+                        ups["{}+".format(tag)] = "lichess"
+                if ups:
+                    tag_coll.update_one({"_id":id},{"$addToSet":ups}, upsert = True)
+                if tags:
+                    puzzle_coll.update_one({"_id":id},{"$addToSet":{"tags":{"$each":tags}}})
+        nb += len(batch)
         if nb % 1000 == 0:
             logger.info(nb)
-
-if __name__ == "__main__":
-    main()
+        batch = []
