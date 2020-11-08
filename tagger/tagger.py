ea8c11ef17cf373ffabb549df47ee2981364c067
Thibault Duplessis <t@lichess.org>
2020-11-08 11:13:49 +0100

    tag with rounds

--- tagger/tagger.py
+++ tagger/tagger.py
@@ -3,10 +3,11 @@ import logging
 import sys
 import argparse
 from multiprocessing import Pool
+from datetime import datetime
 import cook
 from chess import Move, Color, Board, WHITE, BLACK
 from chess.pgn import Game, GameNode
-from typing import List, Optional, Tuple, Literal, Union
+from typing import List, Optional, Tuple, Literal, Union, Dict, Any
 from model import Puzzle, TagKind, static_kinds
 
 logger = logging.getLogger(__name__)
@@ -31,30 +32,38 @@ if args.verbose == 1:
 mongo = pymongo.MongoClient()
 db = mongo['puzzler']
 puzzle_coll = db['puzzle2']
-tag_coll = db['tag']
+round_coll = db['round']
 nb = 0
 
 def tags_of(doc) -> Tuple[Puzzle, List[TagKind]]:
     puzzle = read(doc)
     return puzzle.id, cook.cook(puzzle)
 
+def process_batch(batch: List[Dict[str, Any]]):
+    for id, tags in pool.imap_unordered(tags_of, batch):
+        if not args.dry:
+            round_coll.insert_one({
+                "_id": f"lichess:{id}",
+                "u": "lichess",
+                "p": id,
+                "d": datetime.now(),
+                "w": 10,
+                "t": [f"+{t}" for t in tags if not t in static_kinds]
+            })
+            puzzle_coll.update_one({"_id":id},{"$addToSet":{"tags":{"$each":tags}}})
+
 with Pool(processes=8) as pool:
     batch = []
     for doc in puzzle_coll.find():
+        id = doc["_id"]
+        if round_coll.count_documents({"_id":f"lichess:{id}"}) > 0:
+            continue
         if len(batch) < 200:
             batch.append(doc)
             continue
-        for id, tags in pool.imap_unordered(tags_of, batch):
-            if not args.dry:
-                ups = {}
-                for tag in tags:
-                    if not tag in static_kinds:
-                        ups["{}+".format(tag)] = "lichess"
-                if ups:
-                    tag_coll.update_one({"_id":id},{"$addToSet":ups}, upsert = True)
-                if tags:
-                    puzzle_coll.update_one({"_id":id},{"$addToSet":{"tags":{"$each":tags}}})
+        process_batch(batch)
         nb += len(batch)
         if nb % 1000 == 0:
             logger.info(nb)
         batch = []
+    process_batch(batch)
