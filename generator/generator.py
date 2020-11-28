874e86e56098aef68c57a1f224b21fe405d0e6d8
Thibault Duplessis <t@lichess.org>
2020-11-28 19:58:39 +0100

    v21

--- generator/generator.py
+++ generator/generator.py
@@ -22,8 +22,8 @@ from server import Server
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(levelname)-4s %(message)s', datefmt='%m/%d %H:%M')
 
-version = 20
 get_move_limit = chess.engine.Limit(depth = 50, time = 30, nodes = 40_000_000)
+version = 21
 mate_soon = Mate(15)
 allow_one_mater = True
 allow_one_mover = False
