098bc9530098e858732e5cf1beabc5c52bd202cc
Thibault Duplessis <t@lichess.org>
2020-12-02 12:01:48 +0100

    improve tag and path generators

--- bin/import-more.sh
+++ bin/import-more.sh
@@ -3,11 +3,6 @@
 echo "Download"
 ~/.scripts/fetch-mongodb-collection root@godot.lichess.ovh puzzler puzzle2
 
-echo "Tag"
-cd ~/lichess-puzzler/tagger
-. venv/bin/activate
-python tagger.py
-
 echo "Phase"
 cd ~/lichess-puzzler/phaser
 sbt run
@@ -15,10 +10,18 @@ sbt run
 cd ~/lichess-puzzler
 echo "Copy"
 mongo puzzler bin/copy-to-play.js
+
+echo "Tag"
+cd ~/lichess-puzzler/tagger
+. venv/bin/activate
+python tagger.py
+
 echo "Ratings"
 mongo puzzler bin/random-ratings.js
+
 echo "Votes"
 mongo puzzler bin/random-votes.js
+
 echo "Paths"
 mongo puzzler bin/make-paths.js
 
--- bin/make-paths.js
+++ bin/make-paths.js
@@ -1,6 +1,5 @@
 const puzzleColl = db.puzzle2_puzzle;
 const pathColl = db.puzzle2_path;
-pathColl.remove({});
 
 const generation = Date.now()
 
@@ -26,9 +25,9 @@ function makeTier(theme, tierName, thresholdRatio) {
 
   const nbAcceptablePuzzles = puzzleColl.count(selector);
 
-  if (!nbAcceptablePuzzles) return;
-
   const nbPuzzles = Math.round(nbAcceptablePuzzles * thresholdRatio);
+
+  if (!nbPuzzles) return [];
 
   const pathLength = Math.max(20, Math.min(100, Math.round(nbPuzzles / 100)));
 
@@ -36,7 +35,19 @@ function makeTier(theme, tierName, thresholdRatio) {
 
   print(`theme: ${theme}, tier: ${tierName}, threshold: ${thresholdRatio}, puzzles: ${nbPuzzles}, path length: ${pathLength}, rating buckets: ${nbRatingBuckets}`);
 
-  const ratingBuckets = db.puzzle2_puzzle.aggregate([{
+  // explodeArray([1..12], 3)
+  // [[1, 4, 7, 10], [2, 5, 8, 11], [3, 5, 9, 12]]
+  function explodeArray(arr, nb) {
+    const res = [];
+    for (i = 0; i < nb; i++) res[i] = [];
+    for (i in arr) res[i % nb].push(arr[i]);
+    return res;
+  }
+
+  const docs = [];
+  let bucketNumber = 0;
+
+  db.puzzle2_puzzle.aggregate([{
     $match: selector
   }, {
     $sort: {
@@ -57,19 +68,7 @@ function makeTier(theme, tierName, thresholdRatio) {
   }], {
     allowDiskUse: true,
     comment: 'make-paths'
-  });
-
-  // explodeArray([1..12], 3)
-  // [[1, 4, 7, 10], [2, 5, 8, 11], [3, 5, 9, 12]]
-  function explodeArray(arr, nb) {
-    const res = [];
-    for (i = 0; i < nb; i++) res[i] = [];
-    for (i in arr) res[i % nb].push(arr[i]);
-    return res;
-  }
-
-  let bucketNumber = 0;
-  ratingBuckets.forEach(bucket => {
+  }).forEach(bucket => {
     const ratingMin = Math.ceil(bucket._id.min);
     const ratingMax = ++bucketNumber == nbRatingBuckets ? 9999 : Math.floor(bucket._id.max);
     const nbPaths = Math.floor(bucket.puzzles.length / pathLength);
@@ -77,20 +76,29 @@ function makeTier(theme, tierName, thresholdRatio) {
     const paths = explodeArray(puzzles, nbPaths);
     // print(`  ${ratingMin}->${ratingMax} paths: ${paths.length}`);
     paths.forEach((ids, i) => {
-      pathColl.insert({
+      docs.push({
         _id: `${theme || 'any'}_${tierName}_${ratingMin}-${ratingMax}_${generation}_${i}`,
         tier: tierName,
         theme: theme,
         min: ratingMin,
         max: ratingMax,
         ids,
-        length: ids.length
+        length: ids.length,
+        gen: generation
       });
     });
   });
+
+  return docs;
 }
+
+const docs = [];
 
 themes.concat([null]).forEach(theme =>
 // ['exposedKing'].forEach(theme =>
-  tiers.forEach(([name, threshold]) => makeTier(theme, name, threshold))
+  tiers.forEach(([name, threshold]) => makeTier(theme, name, threshold).forEach(p => docs.push(p)))
 );
+
+pathColl.insert(docs, {ordered: false});
+
+pathColl.remove({gen:{$ne:generation}});
