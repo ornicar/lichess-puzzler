04b5d2b1a2abb242ac558fb65c422b550d40c4ba
Thibault Duplessis <t@lichess.org>
2020-12-07 19:19:10 +0100

    stop doing random votes and ratings

--- bin/import-more.sh
+++ bin/import-more.sh
@@ -11,13 +11,13 @@ echo "Phase"
 cd ~/lichess-puzzler/phaser
 sbt run
 
-echo "Ratings"
-cd ~/lichess-puzzler
-mongo puzzler bin/random-ratings.js
+# echo "Ratings"
+# cd ~/lichess-puzzler
+# mongo puzzler bin/random-ratings.js
 
-echo "Votes"
-cd ~/lichess-puzzler
-mongo puzzler bin/random-votes.js
+# echo "Votes"
+# cd ~/lichess-puzzler
+# mongo puzzler bin/random-votes.js
 
 echo "Themes"
 cd ~/lichess-puzzler
