1f5ed36dc5dc8e606f59c01841a4d3e2d0b41222
Thibault Duplessis <t@lichess.org>
2020-12-09 11:14:47 +0100

    tweak retag

--- bin/retag.sh
+++ bin/retag.sh
@@ -3,7 +3,8 @@
 echo "Themes"
 cd ~/lichess-puzzler/tagger
 . venv/bin/activate
-python tagger.py
+python tagger.py --threads=20
+echo "Zug"
 python tagger.py --zug --threads=16
 
 echo "Themes denormalize"
