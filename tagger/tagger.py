05159401b5ea407fa185e58f445d0d1942af96cc
Thibault Duplessis <t@lichess.org>
2020-12-09 14:14:37 +0100

    tag with more threads

--- tagger/tagger.py
+++ tagger/tagger.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
                 }, upsert = True);
                 play_coll.update_one({"_id":id},{"$set":{"dirty":True}})
 
-    with Pool(processes=16) as pool:
+    with Pool(processes=args.threads) as pool:
         batch: List[Dict[str, Any]] = []
         for doc in play_coll.find():
             id = doc["_id"]
