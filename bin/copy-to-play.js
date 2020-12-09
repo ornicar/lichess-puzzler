d82c49159828f74d48dc8bf9c051922bd1f61aa9
Thibault Duplessis <t@lichess.org>
2020-12-09 10:28:03 +0100

    more efficient copy-to-play

--- bin/copy-to-play.js
+++ bin/copy-to-play.js
@@ -1,23 +1,34 @@
 const buildColl = db.puzzle2;
 const playColl = db.puzzle2_puzzle;
 
+buffer = [];
+
+function process(buf) {
+  const existingIds = new Set(playColl.distinct('_id', {_id:{$in:buf.map(p => p._id)}}));
+  const missing = buf.filter(p => !existingIds.has(p._id)).map(p => ({
+    _id: p._id,
+    gameId: p.gameId,
+    fen: p.fen,
+    themes: [],
+    glicko: {
+      r: 1500,
+      d: 500,
+      v: 0.09
+    },
+    plays: NumberInt(0),
+    vote: NumberInt(1),
+    line: p.moves.join(' ')
+  }));
+  if (missing.length) playColl.insertMany(missing, {ordered:false});
+}
+
 buildColl.find({'review.approved':{$ne:false}}).forEach(p => {
   if (p.moves.length < 2) return;
-  try {
-    playColl.insert({
-      _id: p._id,
-      gameId: p.gameId,
-      fen: p.fen,
-      themes: [],
-      glicko: {
-        r: 1500,
-        d: 500,
-        v: 0.09
-      },
-      plays: NumberInt(0),
-      vote: NumberInt(1),
-      line: p.moves.join(' '),
-      generator: p.generator
-    });
-  } catch {}
+  buffer.push(p);
+  if (buffer.length >= 1000) {
+    process(buffer);
+    buffer = [];
+  }
 });
+
+process(buffer);
