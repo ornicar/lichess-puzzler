d9aec4d940bdd069679d20a564e21cec1cb7f273
Thibault Duplessis <t@lichess.org>
2020-12-10 17:12:45 +0100

    recompute last eval of all puzzles

--- tagger/tagger.py
+++ tagger/tagger.py
@@ -1,7 +1,7 @@
 import pymongo
 import logging
 import argparse
-from multiprocessing import Pool
+from multiprocessing import Process, Queue, Pool, Manager
 from datetime import datetime
 from chess import Move, Board
 from chess.pgn import Game, GameNode
@@ -9,6 +9,7 @@ from chess.engine import SimpleEngine
 from typing import List, Tuple, Dict, Any
 from model import Puzzle, TagKind
 import cook
+import chess.engine
 from zugzwang import zugzwang
 
 logger = logging.getLogger(__name__)
@@ -18,7 +19,7 @@ logger.setLevel(logging.INFO)
 def read(doc) -> Puzzle:
     board = Board(doc["fen"])
     node: GameNode = Game.from_board(board)
-    for uci in doc["line"].split(' '):
+    for uci in (doc["line"].split(' ') if "line" in doc else doc["moves"]):
         move = Move.from_uci(uci)
         node = node.add_main_variation(move)
     return Puzzle(doc["_id"], node.game())
@@ -26,12 +27,14 @@ def read(doc) -> Puzzle:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='tagger.py', description='automatically tags lichess puzzles')
     parser.add_argument("--zug", "-z", help="only zugzwang", action="store_true")
+    parser.add_argument("--eval", "-e", help="only evals", action="store_true")
     parser.add_argument("--dry", "-d", help="dry run", action="store_true")
     parser.add_argument("--all", "-a", help="don't skip existing", action="store_true")
     parser.add_argument("--threads", "-t", help="count of cpu threads for engine searches", default="4")
     args = parser.parse_args()
     mongo = pymongo.MongoClient()
     db = mongo['puzzler']
+    build_coll = db['puzzle2']
     play_coll = db['puzzle2_puzzle']
     round_coll = db['puzzle2_round']
     nb = 0
@@ -56,6 +59,37 @@ if __name__ == "__main__":
             nb += 1
             if nb % 1024 == 0:
                 logger.info(nb)
+        exit(0)
+
+    if args.eval:
+        threads = int(args.threads)
+        eval_nb = 0
+        def cruncher(thread_id: int):
+            global eval_nb
+            build_coll = pymongo.MongoClient()['puzzler']['puzzle2']
+            engine = SimpleEngine.popen_uci('./stockfish')
+            engine.configure({'Threads': 1})
+            engine_limit = chess.engine.Limit(depth = 50, time = 10, nodes = 20_000_000)
+            for doc in build_coll.find({"cp": None}):
+                if ord(doc["_id"][0]) % threads != thread_id:
+                    continue
+                puzzle = read(doc)
+                board = puzzle.game.end().board()
+                if board.is_checkmate():
+                    cp = 999999999
+                    eval_nb += 1
+                    logger.info(f'{thread_id} {eval_nb} {puzzle.id}: mate')
+                else:
+                    info = engine.analyse(board, limit = engine_limit)
+                    score = info["score"].pov(puzzle.pov)
+                    score_cp = score.score()
+                    cp = 999999999 if score.is_mate() else (999999998 if score_cp is None else score_cp)
+                    eval_nb += 1
+                    logger.info(f'{thread_id} {eval_nb} {puzzle.id}: {int(info["nps"] / 1000)} knps -> {cp}')
+                build_coll.update_one({"_id":puzzle.id},{"$set":{"cp":cp}})
+        with Pool(processes=threads) as pool:
+            for i in range(int(args.threads)):
+                Process(target=cruncher, args=(i,)).start()
         exit(0)
 
     def tags_of(doc) -> Tuple[str, List[TagKind]]:
