0b3016d65807f16a1d64683d329b184688286d38
Thibault Duplessis <t@lichess.org>
2021-01-15 10:51:03 +0100

    remove now unused script

--- bin/copy-cp.js
+++
@@ -1,7 +0,0 @@
-const buildColl = db.puzzle2;
-const playColl = db.puzzle2_puzzle;
-
-playColl.find({cp:{$exists:false}}, {_id:true}).forEach(p => {
-  const b = buildColl.findOne({_id:p._id,cp:{$exists:true}}, {cp:true});
-  if (b) playColl.update({_id:p._id},{$set:{cp: b.cp}});
-});
