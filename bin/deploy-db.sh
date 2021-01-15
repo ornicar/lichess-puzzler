dafa11c0274717f93fd2b7534c16e26440b84dfb
Thibault Duplessis <t@lichess.org>
2021-01-15 10:52:05 +0100

    deploy db

---
+++ bin/deploy-db.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+TARGET=root@rubik.lichess.ovh
+DIR=puzzler-dump
+
+rm -rf $DIR
+mongodump --db puzzler --collection puzzle2_puzzle --out $DIR
+mongodump --db puzzler --collection puzzle2_round --out $DIR
+mongodump --db puzzler --collection puzzle2 --out $DIR
+
+rsync -av $DIR $TARGET:/home/puzzler-crom-data
