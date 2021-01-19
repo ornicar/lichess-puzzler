5e9263f17f17adf77fea5236ef96f446dad22a27
Thibault Duplessis <t@lichess.org>
2021-01-19 09:26:57 +0100

    script tweaks

--- bin/download-supergm-games.sh
+++ bin/download-supergm-games.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-OUT=data/supergm-games.pgn
+perf=blitz
+OUT=data/supergm-games-$perf.pgn
 rm $OUT
 
 supergms='DrNykterstein DrDrunkenstein DannyTheDonkey ManWithaVan Bombegranate avalongamemaster Konevlad alireza2003 Azerichessss Moose959 AnishGiri AnishOnYoutube GMVallejo Vladimirovich9000 EvilGenius94 BakukaDaku87 Wesley_So STL_So gmwesleyso1993 RealDavidNavara Dr-BassemAmin gmluke howitzer14 Benefactorr IGMGataKamsky dalmatinac101 Vladimir201701 LiviuDieterNisipeanu jitanu76 ArkadijNaiditsch R-Ponomariov athena-pallada Chepursias muisback VerdeNotte KeyzerSose NeverEnough Sasha jlhammer'
 
 for u in $supergms; do
   echo $u
-  curl https://lichess.org/api/games/user/$u\?perf\=bullet\&rated\=true\&analysed\=true\&clocks=false\&evals\=true >> $OUT
+  curl https://lichess.org/api/games/user/$u\?perfType\=$perf\&rated\=true\&analysed\=true\&clocks=false\&evals\=true >> $OUT
 done
--- bin/master-theme.sh
+++ bin/master-theme.sh
@@ -5,8 +5,8 @@ DIR=puzzler-dump
 mongodump --db puzzler --collection puzzle2_puzzle --out $DIR
 mongodump --db puzzler --collection puzzle2_round --out $DIR
 
-mongorestore --db lichess --collection puzzle2_puzzle $DIR/puzzler/puzzle2_puzzle.bson | grep -v duplicate
-mongorestore --db lichess --collection puzzle2_round $DIR/puzzler/puzzle2_round.bson | grep -v duplicate
+mongorestore --db lichess --collection puzzle2_puzzle $DIR/puzzler/puzzle2_puzzle.bson 2> >(grep -v duplicate)
+mongorestore --db lichess --collection puzzle2_round $DIR/puzzler/puzzle2_round.bson 2> >(grep -v duplicate)
 
 mongo lichess bin/update-puzzle2_master.js
 
