5c0a66f4941d9b0b49067bd4cb77af4c855dae14
Thibault Duplessis <t@lichess.org>
2022-06-06 10:02:39 +0200

    phaser openings WIP - best done in lila probably

--- phaser/src/main/scala/Main.scala
+++ phaser/src/main/scala/Main.scala
@@ -2,14 +2,14 @@ package lichess
 
 import akka.actor.ActorSystem
 import akka.stream.scaladsl._
-import chess.format.{FEN, Forsyth, Uci}
+import chess.format.{ FEN, Forsyth, Uci }
+import chess.opening.FullOpening
 import reactivemongo.akkastream.cursorProducer
 import reactivemongo.api._
 import reactivemongo.api.bson._
 import reactivemongo.api.bson.collection.BSONCollection
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-
 
 object Main extends App {
 
@@ -18,20 +18,22 @@ object Main extends App {
   val mongoUri = "mongodb://localhost:27017"
 
   // Connect to the database: Must be done only once per application
-  val driver = AsyncDriver()
+  val driver    = AsyncDriver()
   val parsedUri = MongoConnection.fromString(mongoUri)
 
   // Database and collections: Get references
-  val futureConnection = parsedUri.flatMap(driver.connect(_))
-  def db: Future[DB] = futureConnection.flatMap(_.database("puzzler"))
-  def puzzleColl = db.map(_.collection("puzzle2_puzzle"))
+  val futureConnection     = parsedUri.flatMap(driver.connect(_))
+  def puzzleDb: Future[DB] = futureConnection.flatMap(_.database("puzzler"))
+  def puzzleColl           = puzzleDb.map(_.collection("puzzle2_puzzle"))
+  def gameDb: Future[DB]   = futureConnection.flatMap(_.database("lichess"))
+  def gameColl             = puzzleDb.map(_.collection("game5"))
 
   case class Puzzle(id: String, fen: FEN, move: String)
 
   def read(doc: BSONDocument): Puzzle = {
     for {
-      id <- doc.string("_id")
-      fen <- doc.string("fen").map(FEN.apply)
+      id   <- doc.string("_id")
+      fen  <- doc.string("fen").map(FEN.apply)
       move <- doc.getAsOpt[String]("line").map(_.takeWhile(' ' !=))
     } yield Puzzle(id, fen, move)
   } getOrElse sys.error(s"Can't read BSONDocument ${doc.string("_id")}")
@@ -39,11 +41,13 @@ object Main extends App {
   def phaseOf(puzzle: Puzzle): String = {
     for {
       situation <- Forsyth.<<(puzzle.fen)
-      uci <- Uci.Move(puzzle.move)
-      next <- situation.move(uci).toOption
+      uci       <- Uci.Move(puzzle.move)
+      next      <- situation.move(uci).toOption
       board = next.finalizeAfter
     } yield Divider(board)
   } getOrElse sys.error(s"Can't make board for $puzzle")
+
+  def openingOf(puzzle: Puzzle): Future[FullOpening] = ??? // FullOpeningDB.search
 
   def update(coll: BSONCollection, puzzle: Puzzle, phase: String) = {
     // coll.update.one(
@@ -55,7 +59,7 @@ object Main extends App {
     //   )
     // ) flatMap { _ =>
     coll.update.one(
-      BSONDocument("_id" -> puzzle.id),
+      BSONDocument("_id"       -> puzzle.id),
       BSONDocument("$addToSet" -> BSONDocument("themes" -> phase))
     )
   }
@@ -65,16 +69,25 @@ object Main extends App {
       coll
         .find(
           BSONDocument(
-            "themes" -> BSONDocument(
-              "$nin" -> List("opening", "middlegame", "endgame")
+            "$or" -> BSONArray(
+              BSONDocument(
+                "themes" -> BSONDocument(
+                  "$nin" -> List("opening", "middlegame", "endgame")
+                )
+              ),
+              BSONDocument(
+                "opening" -> BSONDocument("$exists" -> false)
+              )
             )
           )
         )
         .cursor[BSONDocument]()
         .documentSource()
         .map(read)
-        .map(p => p -> phaseOf(p))
-        .mapAsyncUnordered(12) { case (p, phase) =>
+        .mapAsyncUnordered(16) { p =>
+          openingOf(p) map { opening => (p, phaseOf(p), opening) }
+        }
+        .mapAsyncUnordered(16) { case (p, phase, opening) =>
           update(coll, p, phase)
         }
         .toMat(Sink.ignore)(Keep.right)
