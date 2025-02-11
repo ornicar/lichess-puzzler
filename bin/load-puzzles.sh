24599e689f70f33ca22d2ee8f96e79cf10febbd5
Thibault Duplessis <t@lichess.org>
2025-02-11 10:14:58 +0100

    update load-puzzles.sh

--- bin/load-puzzles.sh
+++ bin/load-puzzles.sh
@@ -2,7 +2,9 @@
 # This script belongs on rubik:/home/puzzler-crom-data/puzzler-dump/load-puzzles.sh
 # And is called by deploy-db.sh
 
-mongorestore mongodb://localhost:27017 --db puzzler --collection puzzle2_puzzle puzzler/puzzle2_puzzle.bson --writeConcern '{w:0}'
-mongorestore mongodb://localhost:27017 --db puzzler --collection puzzle2_round puzzler/puzzle2_round.bson --writeConcern '{w:0}'
-mongorestore mongodb://localhost:27017 --db puzzler --collection puzzle2_blocklist puzzler/puzzle2_blocklist.bson --writeConcern '{w:0}'
-mongo puzzler --eval 'db.puzzle2_blocklist.find().forEach(p => db.puzzle2_puzzle.deleteOne(p))'
+options="--db puzzler --writeConcern '{w:0}' --noIndexRestore"
+
+mongorestore mongodb://localhost:27017 --collection puzzle2_puzzle puzzler/puzzle2_puzzle.bson $options
+mongorestore mongodb://localhost:27017 --collection puzzle2_round puzzler/puzzle2_round.bson $options
+mongorestore mongodb://localhost:27017 --collection puzzle2_blocklist puzzler/puzzle2_blocklist.bson $options
+mongosh puzzler --eval 'db.puzzle2_blocklist.find().forEach(p => db.puzzle2_puzzle.deleteOne(p))'
